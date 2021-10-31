0dcfbb25f5117fadf6b02535efbff4867f19d3cb
SyedShajib <syedshajib55@gmail.com>
2021-10-31 18:44:13 +0600

    final

--- src/Components/Videos/Videos.js
+++ src/Components/Videos/Videos.js
@@ -6,7 +6,7 @@ const Videos = () => {
             <div>
                 <h1 className='p-2 featured m-2'>Featured Videos</h1>
             </div>
-            <div className="videos" >
+            <div className="videos row col-md-6 col-sm-12" >
                 <div>
                     <iframe className="m-3" width="560" height="315" src="https://www.youtube.com/embed/Qmi-Xwq-MEc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
